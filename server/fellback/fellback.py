00be89789a1e5bae868c2dc214613c42ba3f1dcb
etveritas <bravewavy@gmail.com>
2023-08-29 12:26:35 +0800

    修改示例内容

--- server/fellback/fellback.py
+++ server/fellback/fellback.py
@@ -18,7 +18,7 @@ fell_logger.addHandler(handler)
 def fellback_save(uuid: str = Body(..., description="该组对话唯一标识", examples=["2c24de88"]),
                   answer: str = Body(..., description="大模型回答内容", examples=["你好，有什么可以帮助你的吗？"]),
                   docs: List[str] = Body(..., description="相似度知识文档列表", examples=[["你好1", "你好2"]]),
-                  prompt: str = Body(..., description="用户问题组装的Prompt", examples=["请根据一下内容回答问题：你好"]),
+                  prompt: str = Body(..., description="用户问题组装的Prompt", examples=["请根据以下内容回答问题：你好"]),
                   fellback: str = Body(..., description="用户反馈内容", examples=["回答的准确"]),
                   ):
     # 日志文件路径
