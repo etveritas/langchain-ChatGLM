904368c66e6d80a72a0a0d342ee120bd0e28c08a
etveritas <bravewavy@gmail.com>
2023-08-30 17:27:59 +0800

    增加专家聊天接口（场景定制化）

--- server/api.py
+++ server/api.py
@@ -12,7 +12,7 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from server.chat import (chat, knowledge_base_chat, openai_chat,
-                         search_engine_chat)
+                         search_engine_chat, expert_chat)
 from server.knowledge_base.kb_api import list_kbs, create_kb, delete_kb
 from server.knowledge_base.kb_doc_api import (list_docs, upload_doc, delete_doc,
                                               update_doc, download_doc, recreate_vector_store,
@@ -62,6 +62,10 @@ def create_app():
     app.post("/chat/knowledge_base_chat",
              tags=["Chat"],
              summary="与知识库对话")(knowledge_base_chat)
+
+    app.post("/chat/expert_chat",
+             tags=["Chat"],
+             summary="与领域专家对话")(expert_chat)
 
     app.post("/chat/search_engine_chat",
              tags=["Chat"],
---
+++ server/chat/expert_chat.py
@@ -0,0 +1,137 @@
+from fastapi import Body, Request
+from fastapi.responses import StreamingResponse
+from configs.model_config import (llm_model_dict, LLM_MODEL, PROMPT_TEMPLATE,
+                                  VECTOR_SEARCH_TOP_K, SCORE_THRESHOLD, RECOGNIZE_PROMPT)
+from server.chat.utils import wrap_done
+from server.utils import BaseResponse
+from langchain.chat_models import ChatOpenAI
+from models.chatglm import ChatChatGLM
+from langchain.llms import ChatGLM, OpenAI
+from langchain import LLMChain
+from langchain.output_parsers import CommaSeparatedListOutputParser
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from typing import AsyncIterable, List, Optional
+import asyncio
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from server.chat.utils import History
+from server.knowledge_base.kb_service.base import KBService, KBServiceFactory
+import json
+import os
+import uuid
+import numpy as np
+from urllib.parse import urlencode
+from server.knowledge_base.kb_doc_api import search_docs
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def expert_chat(query: str = Body(..., description="用户输入", examples=["你好"]),
+                        knowledge_base_name: str = Body(..., description="知识库名称", examples=["samples"]),
+                        top_k: int = Body(VECTOR_SEARCH_TOP_K, description="匹配向量数"),
+                        score_threshold: float = Body(SCORE_THRESHOLD, description="知识库匹配相关度阈值，取值范围在0-1之间，SCORE越小，相关度越高，取到1相当于不筛选，建议设置在0.5左右", ge=0, le=1100),
+                        history: List[History] = Body([],
+                                                      description="历史对话",
+                                                      examples=[[
+                                                          {"role": "user",
+                                                           "content": "我们来玩成语接龙，我先来，生龙活虎"},
+                                                          {"role": "assistant",
+                                                           "content": "虎头虎脑"}]]
+                                                      ),
+                        stream: bool = Body(False, description="流式输出"),
+                        local_doc_url: bool = Body(False, description="知识文件返回本地路径(true)或URL(false)"),
+                        request: Request = None,
+                        ):
+    kb = KBServiceFactory.get_service_by_name(knowledge_base_name)
+    if kb is None:
+        return BaseResponse(code=404, msg=f"未找到知识库 {knowledge_base_name}")
+
+    history = [History(**h) if isinstance(h, dict) else h for h in history]
+
+    async def expert_chat_iterator(query: str,
+                                           kb: KBService,
+                                           top_k: int,
+                                           history: Optional[List[History]],
+                                           ) -> AsyncIterable[str]:
+        callback = AsyncIteratorCallbackHandler()
+        if "gpt" in LLM_MODEL:
+            model = ChatOpenAI(
+                top_p=0.9,
+                temperature=0.6,
+                streaming=True,
+                verbose=True,
+                callbacks=[callback],
+                openai_api_key=llm_model_dict[LLM_MODEL]["api_key"],
+                openai_api_base=llm_model_dict[LLM_MODEL]["api_base_url"],
+                model_name=LLM_MODEL
+            )
+        elif "glm" in LLM_MODEL:
+            model = ChatChatGLM(
+                top_p=0.9,
+                temperature=0.6,
+                streaming=True,
+                verbose=True,
+                callbacks=[callback],
+                chatglm_api_key=llm_model_dict[LLM_MODEL]["api_key"],
+                chatglm_api_base=llm_model_dict[LLM_MODEL]["api_base_url"],
+                model_name=LLM_MODEL
+            )
+
+        output_parser = CommaSeparatedListOutputParser()
+        chatp = PromptTemplate(template=RECOGNIZE_PROMPT, input_variables=["user_query"], output_parser=output_parser)
+        # recg_prompt = chatp.from_messages([("human", RECOGNIZE_PROMPT)])
+        chain_recg = LLMChain(prompt=chatp, llm=model)
+        qry_kw = chain_recg.predict(user_query=query)
+        print("qry_kw:", qry_kw)
+        docs = search_docs(query, knowledge_base_name, top_k, score_threshold)
+        context = "\n".join([doc.page_content for doc in docs])
+
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [i.to_msg_tuple() for i in history] + [("human", PROMPT_TEMPLATE)])
+
+        chain = LLMChain(prompt=chat_prompt, llm=model)
+
+        # combine prompt
+        prompt_comb = await chain.aprep_prompts([{"context": context, "question": query}])
+
+        # Begin a task that runs in the background.
+        task = asyncio.create_task(wrap_done(
+            chain.acall({"context": context, "question": query}),
+            callback.done),
+        )
+
+        source_documents = []
+        for inum, doc in enumerate(docs):
+            filename = os.path.split(doc.metadata["source"])[-1]
+            if local_doc_url:
+                url = "file://" + doc.metadata["source"]
+            else:
+                parameters = urlencode({"knowledge_base_name": knowledge_base_name, "file_name":filename})
+                url = f"{request.base_url}knowledge_base/download_doc?" + parameters
+            text = f"""出处 [{inum + 1}] [{filename}]({url}) \n\n{doc.page_content}\n\n 相似度：{1100-doc.score}\n\n"""
+            source_documents.append(text)
+
+        unq_id = uuid.uuid1()
+        if stream:
+            async for token in callback.aiter():
+                # Use server-sent-events to stream the response
+                yield json.dumps({"uuid": str(unq_id),
+                                  "answer": token,
+                                  "docs": source_documents,
+                                  "prompt": prompt_comb[0][0].to_string()},
+                                 ensure_ascii=False)
+        else:
+            answer = ""
+            async for token in callback.aiter():
+                answer += token
+            yield json.dumps({"uuid": str(unq_id),
+                              "answer": answer,
+                              "docs": source_documents,
+                              "prompt": prompt_comb[0][0].to_string()},
+                             ensure_ascii=False)
+
+        await task
+
+    return StreamingResponse(expert_chat_iterator(query, kb, top_k, history),
+                             media_type="text/event-stream")
